171ff222bf59a529e279256d16a53e590cd97eaf
Nitesh Ranjan <116751667+Nitesh1234-ranjan@users.noreply.github.com>
2025-10-10 21:05:59 +0530

    Update ReportController.java
    Remove CORS

--- src/main/java/com/example/finance_tracker/controller/ReportController.java
+++ src/main/java/com/example/finance_tracker/controller/ReportController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 @RequestMapping(ApiPaths.REPORTS)
 public class ReportController {
